4c9a99fd9ba92c30fbea0f0892db4bcea82a6ce2
Dreamyyy-rin <oivicko020405@gmail.com>
2024-11-28 15:08:06 +0700

    penambahan db

--- src/restaurantjavaapp/model/Koneksi.java
+++ src/restaurantjavaapp/model/Koneksi.java
@@ -4,10 +4,35 @@
  */
 package restaurantjavaapp.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 /**
  *
  * @author User
  */
 public class Koneksi {
-    
+
+    public Connection con;
+    public Statement stm;
+
+    //method untuk konfigurasi koneksi ke db
+    public void config() {
+        try {
+            String url = "jdbc:mysql://127.0.0.1/db_restoran";
+            String user = "root";
+            String pass = "";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            //string connection
+            con = DriverManager.getConnection(url, user, pass);
+
+            //mempersiapkan variabel untuk sql statement
+            stm = (Statement) con.createStatement();
+            System.out.println("Koneksi Berhasil");
+        } catch (Exception e) {
+            System.out.println("Koneksi Gagal" + e);
+        }
+    }
 }
